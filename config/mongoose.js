208dff0216016f36e4184216c553a91ea088073d
luck22y <luckykumar2204@gmail.com>
2023-06-15 16:57:08 +0530

    final commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,12 +4,12 @@ mongoose.connect(
   "mongodb+srv://admin22:tabtab123@cluster0.9mlsgec.mongodb.net/nodejs_auth?retryWrites=true&w=majority"
 );
 
-const db = mongoose.connect;
+const db = mongoose.connection;
 
-db.on("error", console.log.bind("Error connecting to MongoDB"));
+db.on("error", console.log.bind(`Error connecting to MongoDB`));
 
 db.once("open", function () {
-  console.log("MongoDB Connected");
+  console.log(`Connected to MongoDB`);
 });
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const mongoose = require("mongoose");
-const flash = require("connect-flash");
-const session = require("express-session");
-const passport = require("passport");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 const db = require("./config/mongoose");
+const passport = require('passport');
 
-const port = process.env.PORT || 8000;
 const app = express();
 
 //------------ Passport Configuration ------------//
 require('./config/passport')(passport);
 
-//------------ Bodyparser Configuration ------------//
-app.use(express.urlencoded({ extended: false }));
-
 //------------ EJS Configuration ------------//
-app.use(expressLayouts)
-app.use('/assets',express.static('./assets'))
-app.use('view engine', 'ejs')
+app.use(expressLayouts);
+app.use("/assets", express.static('./assets'));
+app.set('view engine', 'ejs');
+
+//------------ Bodyparser Configuration ------------//
+app.use(express.urlencoded({ extended: false }))
+
+//------------ Express session Configuration ------------//
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
 
 //------------ Passport Middlewares ------------//
+app.use(passport.initialize());
+app.use(passport.session());
 
 //------------ Connecting flash ------------//
 app.use(flash());
 
 //------------ Global variables ------------//
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   next();
 });
-
 //------------ Routes ------------//
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
-//------------ Express session Configuration ------------//
-app.use(
-    session({
-        secret:'secret',
-        resave:true,
-        saveUninitialized:true
-    })
-)
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, function (err) {
-  if (err) {
-    console.log("Error Starting the Server");
-  }
-  console.log(`Server up & running on port: ${port}`);
-});
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
